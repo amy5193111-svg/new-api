061b41579888c5a46faba617f562d19b9bb164fa
amy5193111-svg <amy5193111@gmail.com>
2026-09-06 17:49:46 +0800

    fix: paymentReturnPath prefer CustomCallbackAddress over localhost ServerAddress

--- controller/return_path.go
+++ controller/return_path.go
@@ -3,10 +3,15 @@ package controller
 import (
 	"strings"
 
+	"github.com/QuantumNous/new-api/setting/operation_setting"
 	"github.com/QuantumNous/new-api/setting/system_setting"
 )
 
 func paymentReturnPath(suffix string) string {
-	base := strings.TrimRight(system_setting.ServerAddress, "/")
+	// 优先使用自定义回调地址（外网可访问），否则回退到服务器地址
+	base := strings.TrimRight(operation_setting.CustomCallbackAddress, "/")
+	if base == "" {
+		base = strings.TrimRight(system_setting.ServerAddress, "/")
+	}
 	return base + suffix
 }
